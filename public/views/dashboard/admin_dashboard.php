c966adfd403a7e697be9d25c015a341baa04965b
LeonardoPontini <168545949+LeonardoPontini@users.noreply.github.com>
2026-02-10 12:17:17 +0100

    fix admin

--- public/views/dashboard/admin_dashboard.php
+++ public/views/dashboard/admin_dashboard.php
@@ -1,8 +1,4 @@
 <?php
-// DEBUG TEMPORANEO
-// ini_set('display_errors', 1);
-// error_reporting(E_ALL);
-// echo "<!-- DEBUG: admin_dashboard.php loaded -->";
 use App\configurationDB\Database;
 require_once __DIR__ . '/../header.php';
 require_once __DIR__ . '/../../../vendor/autoload.php';
