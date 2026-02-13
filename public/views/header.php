6447288d6dbbd45ff47028093bf4b3155e72dc24
LeonardoPontini <168545949+LeonardoPontini@users.noreply.github.com>
2026-02-13 16:52:46 +0100

    modifica header

--- public/views/header.php
+++ public/views/header.php
@@ -17,19 +17,30 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm py-2">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">ESG Balance</a>
+            <a class="navbar-brand fs-2 fw-bold text-success" href="/">ESG Balance</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto align-items-center">
+
+                    <li class="nav-item me-lg-4 mb-2 mb-lg-0">
+                        <a class="btn btn-outline-primary fw-bold" href="/views/statistiche.php">
+                            Statistiche Pubbliche
+                        </a>
+                    </li>
 
                     <?php if (isset($_SESSION['user'])): ?>
-                        <li class="nav-item">
-                            <span class="nav-link">Ciao, <?php echo $_SESSION['user'] ?></span>
+                        <li class="nav-item me-lg-3">
+                            <span class="nav-link fw-semibold">Ciao,
+                                <?php echo htmlspecialchars($_SESSION['user']); ?></span>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link btn btn-danger text-white btn-sm" href="/views/logout.php">Esci</a>
+                            <a class="btn btn-danger text-white btn-sm" href="/views/logout.php">Esci</a>
                         </li>
                     <?php endif; ?>
 
